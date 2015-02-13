2f57a78720ef2808f2616f7cadb8e4bede7c0525
unknown <igor.di.paolo@gmail.com>
2015-02-13 16:21:12 +0100

    Error model added

--- model/channel/hr-wpan-error-model.h
+++ model/channel/hr-wpan-error-model.h
@@ -24,41 +24,41 @@
 
 namespace ns3 {
 
-/**
- * \ingroup hr-wpan
- *
- * Model the error rate for IEEE 802.15.4 2.4 GHz AWGN channel for OQPSK
- * the model description can be found in IEEE Std 802.15.4-2006, section
- * E.4.1.7
- */
-class HrWpanErrorModel : public Object
-{
-public:
-  /**
-   * Get the type ID.
-   *
-   * \return the object TypeId
-   */
-  static TypeId GetTypeId (void);
+	/**
+	 * \ingroup hr-wpan
+	 *
+	 * Model the error rate for IEEE 802.15.4 2.4 GHz AWGN channel for OQPSK
+	 * the model description can be found in IEEE Std 802.15.4-2006, section
+	 * E.4.1.7
+	 */
+	class HrWpanErrorModel : public Object
+	{
+	public:
+		/**
+		 * Get the type ID.
+		 *
+		 * \return the object TypeId
+		 */
+		static TypeId GetTypeId(void);
 
-  HrWpanErrorModel (void);
+		HrWpanErrorModel(void);
 
-  /**
-   * Return chunk success rate for given SNR.
-   *
-   * \return success rate (i.e. 1 - chunk error rate)
-   * \param snr SNR expressed as a power ratio (i.e. not in dB)
-   * \param nbits number of bits in the chunk
-   */
-  double GetChunkSuccessRate (double snr, uint32_t nbits) const;
+		/**
+		 * Return chunk success rate for given SNR.
+		 *
+		 * \return success rate (i.e. 1 - chunk error rate)
+		 * \param snr SNR expressed as a power ratio (i.e. not in dB)
+		 * \param nbits number of bits in the chunk
+		 */
+		double GetChunkSuccessRate(double snr, uint32_t nbits) const;
 
-private:
-  /**
-   * Array of precalculated binomial coefficients.
-   */
-  double m_binomialCoefficients[17];
+	private:
+		/**
+		 * Array of precalculated binomial coefficients.
+		 */
+		double m_binomialCoefficients[17];
 
-};
+	};
 
 
 } // namespace ns3
---
+++ model/channel/hr-wpan-spectrum-signal-parameters.cc
@@ -0,0 +1,45 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+* Copyright (c) 2012 The Boeing Company
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation;
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+* Author: Gary Pei <guangyu.pei@boeing.com>
+*/
+
+#include "hr-wpan-spectrum-signal-parameters.h"
+#include <ns3/log.h>
+#include <ns3/packet-burst.h>
+
+namespace ns3 {
+	NS_LOG_COMPONENT_DEFINE("HrWpanSpectrumSignalParameters");
+
+	HrWpanSpectrumSignalParameters::HrWpanSpectrumSignalParameters(void)
+	{
+		NS_LOG_FUNCTION(this);
+	}
+
+	HrWpanSpectrumSignalParameters::HrWpanSpectrumSignalParameters(const HrWpanSpectrumSignalParameters& p)
+		: SpectrumSignalParameters( p)
+	{
+		NS_LOG_FUNCTION(this << &p);
+		packetBurst = p.packetBurst->Copy();
+	}
+
+	Ptr<SpectrumSignalParameters> HrWpanSpectrumSignalParameters::Copy(void)
+	{
+		NS_LOG_FUNCTION(this);
+		return Create<HrWpanSpectrumSignalParameters>(*this);
+	}
+}
